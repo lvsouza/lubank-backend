3f842507e49e86ec3a1c2f37440a1577deb22b04
lvsouza <ls18017@gmail.com>
2020-08-26 20:17:25 -0300

    refactor: code improvements

--- src/database/TableNames.ts
+++ src/database/TableNames.ts
@@ -1,3 +1,3 @@
 export enum TableNames {
-    user="user",
+    user = "user",
 }
--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 
 const route = express.Router();
-route.get('/', (_, res) => res.json("Working..."));
+route.get('/', (_, res) => res.json("Lubank is working..."));
 
 export const routes = route;
